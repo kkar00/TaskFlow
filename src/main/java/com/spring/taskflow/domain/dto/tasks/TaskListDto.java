4bf51e41ad85a9588a86a0c7bf893aded389ed1c
mong9ssi <mong9ssi@naver.com>
2025-06-17 15:42:06 +0900

    add: 태스크 조회 API 구현 page 구현완료 / createById 추가 필요함

---
+++ src/main/java/com/spring/taskflow/domain/dto/tasks/TaskListDto.java
@@ -0,0 +1,78 @@
+package com.spring.taskflow.domain.dto.tasks;
+
+import com.spring.taskflow.domain.entity.Task;
+import com.spring.taskflow.domain.enumdata.Priority;
+import com.spring.taskflow.domain.enumdata.Status;
+
+import java.time.LocalDateTime;
+
+public class TaskListDto {
+    private Long taskId;
+    private String title;
+    private String description;
+    private Priority priority;
+    private Long createdById;
+    private Long assigneeId;
+    private LocalDateTime startDate;
+    private LocalDateTime dueDate;
+    private Status status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public TaskListDto(Task task) {
+        this.taskId = task.getTaskId();
+        this.title = task.getTitle();
+        this.description = task.getDescription();
+        this.priority = task.getPriority();
+        this.assigneeId = task.getAssigneeId().getUserId();
+        this.startDate = task.getStartDate();
+        this.dueDate = task.getDueDate();
+        this.status = task.getStatus();
+        this.createdAt = task.getCreatedAt();
+        this.updatedAt = task.getUpdatedAt();
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public Long getCreatedById() {
+        return createdById;
+    }
+
+    public Long getAssigneeId() {
+        return assigneeId;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
